fd7d8f977c54f1884b910bb7b8512ffc01dae5df
Xiangling Liu <anitaliubfsu@gmail.com>
2025-04-24 15:28:57 -0700

    the app now uses my model to make predictions

--- labs/lab8/lab8app/main.py
+++ labs/lab8/lab8app/main.py
@@ -1,19 +1,20 @@
 # lab8app/main.py
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from fastapi import FastAPI
+import mlflow.sklearn
+from mlflow.tracking import MlflowClient
+import pandas as pd
 import joblib
 
 app = FastAPI()
 
-# Load model once at startup
-model = joblib.load("reddit_model_pipeline.joblib")
+# Load model from MLflow
+model = joblib.load("../model.joblib")
 
-class RedditComment(BaseModel):
-    text: str
-
-@app.post("/predict")
-def predict(comment: RedditComment):
-    # Predict probability of removal (1 = removed, 0 = not removed)
-    proba = model.predict_proba([comment.text])[0]
-    pred = int(proba[1] > 0.5)
-    return {"prediction": pred, "probability_removed": proba[1]}
+# Endpoint to trigger predictions on a fixed CSV
+@app.get("/predict")
+def predict():
+    df = pd.read_csv("x_test_scaled.csv")
+    if "Unnamed: 0" in df.columns:
+        df = df.drop(columns=["Unnamed: 0"])
+    pred = model.predict(df.sample(1, random_state=42)).tolist()
+    return {"predictions": pred}
--- labs/lab8/lab8app/test_app.py
+++ labs/lab8/lab8app/test_app.py
@@ -1,8 +1,5 @@
 # lab8app/test_app.py
 import requests
 
-data = {"text": "This comment might get removed!"}
-response = requests.post("http://127.0.0.1:8000/predict", json=data)
-
-print("Response from API:")
-print(response.json())
+response = requests.get("http://127.0.0.1:8000/predict")
+print("Predictions:", response.json())
